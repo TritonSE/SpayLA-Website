0bd9fbedbb3d262db75a52ccc86aefc1f07484d9
Syu125 <yusophia2011@gmail.com>
2025-01-29 08:40:18 -0800

    Added animation to cards

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,17 @@
 import styles from "@/app/page.module.css";
 import HoverCard from "@/components/hovercard/hovercard";
-import backgroundImage1 from "../assets/market_plan/local_orgs.jpg";
 import { cardDetails } from "@/components/hovercard/carddetail";
 
 export default function Home() {
   return (
     <main>
+      <div className={styles.sectionHeader}>
+        <h1 className={styles.sectionTitle}>Marketing Plan</h1>
+        <p className={styles.sectionDescription}>
+          Here are several petitions that you can help sign to create further traction on
+          legislation and proper funding for spay/neuter clinics.
+        </p>
+      </div>
       <div className={styles.grid}>
         {cardDetails.map((card, index) => (
           <HoverCard
