ef04430bbb0a3a59a5878a1c48855d65ca4bd3c1
b-jonathan <ribellye@gmail.com>
2025-02-02 00:14:05 -0800

    Main, Investment, Appointment page completed
    Finished statement, investment and appointment page.
    
    Added Montserrat font into globals.css
    
    Added chartjs library for bar graph

--- frontend/src/components/solution/Appointments.tsx
+++ frontend/src/components/solution/Appointments.tsx
@@ -1,7 +1,88 @@
+"use client";
 import React from "react";
 import styles from "@/components/solution/appointments.module.css";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  Colors,
+} from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ChartDataLabels);
 
 const Appointments: React.FC = () => {
+  const data = {
+    labels: [
+      ["Other", "Clinics"],
+      ["Spay.LA", "Clinics"],
+    ],
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: [11520, 28000],
+        backgroundColor: ["#2D5177", "#ff8359"],
+        borderColor: ["#2D5177", "#ff8359"],
+        borderWidth: 1,
+        barThickness: 80,
+      },
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        top: 75,
+      },
+    },
+    plugins: {
+      tooltip: {
+        enabled: false,
+      },
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        anchor: "end",
+        align: "top",
+        offset: 5,
+        color: ["#2D5177", "#ff8359"],
+        font: {
+          size: 24,
+          weight: "bold",
+          family: "Montserrat",
+        },
+        formatter: (value) => value.toLocaleString(), // Format numbers with commas
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          drawOnChartArea: false,
+          drawBorder: false,
+        },
+        ticks: {
+          color: "black",
+          font: {
+            size: 20,
+            family: "Montserrat",
+          },
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+        display: false,
+      },
+    },
+  };
+
   return (
     <div className={styles.outerContainer}>
       <div className={styles.innerContainer}>
@@ -19,6 +100,7 @@ const Appointments: React.FC = () => {
         </div>
         <div className={styles.graphSection}>
           <p className={styles.graphTitle}>Surgeries Per Year</p>
+          <Bar data={data} options={options} />
         </div>
       </div>
     </div>
