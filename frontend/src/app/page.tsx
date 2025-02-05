896e1a620e98e7db712af53cac51bca297ded606
Syu125 <yusophia2011@gmail.com>
2025-02-05 13:42:32 -0800

    Moved page code to its separate component files

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,29 +1,9 @@
-import styles from "@/app/page.module.css";
-import HoverCard from "@/components/hovercard/hovercard";
-import { cardDetails } from "@/components/hovercard/carddetail";
-// import globalstyle from "@/app/globals.css";
+import MarketSection from "@/components/market_plan/marketSection";
 
 export default function Home() {
   return (
     <main>
-      <div className={styles.sectionHeader}>
-        <h1 className="sectionTitle">Marketing Plan</h1>
-        <p className={styles.sectionDescription}>
-          Here are several petitions that you can help sign to create further traction on
-          legislation and proper funding for spay/neuter clinics.
-        </p>
-      </div>
-      <div className={styles.grid}>
-        {cardDetails.map((card, index) => (
-          <HoverCard
-            backgroundImage={card.backgroundImage}
-            cardTitle={card.cardTitle}
-            cardDescription={card.cardDescription}
-            backgroundColor={card.backgroundColor}
-            hoverColor={card.hoverColor}
-          />
-        ))}
-      </div>
+      <MarketSection></MarketSection>
     </main>
   );
 }
