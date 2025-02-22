07da4505999fc1ed6bdfbe12c72331318f31dc08
anirudh9280 <anirudh.annabathula@gmail.com>
2025-02-22 04:28:53 -0800

    CallPoliticians component but css is messed up on shrink-res

---
+++ frontend/src/app/CallPoliticians.tsx
@@ -0,0 +1,30 @@
+import styles from "./CallPoliticians.module.css";
+
+export default function CallPoliticians() {
+  return (
+    <div className={styles.grid}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Call your local politicians!</h1>
+        <p className={styles.subtitle}>
+          By calling local politicians, we can help get many important issues in the forefront of
+          politician's agendas and budgets.
+        </p>
+        <div className={styles.politicianGrid}>
+          {[...Array(4)].map((_, index) => (
+            <div key={index}>
+              <div className={styles.circle}></div>
+              <div className={styles.politicianInfo}>
+                <div className={styles.politicianName}>John Smith</div>
+                <div className={styles.politicianDetails}>
+                  District 46 Representative
+                  <br />
+                  123-456-7890
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "@/app/page.module.css";
 import HelpOutToday from "./HelpOutToday";
 import SampleScript from "./SampleScript";
+import CallPoliticians from "./CallPoliticians";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <HelpOutToday />
       <SampleScript />
+      <CallPoliticians />
     </main>
   );
 }
