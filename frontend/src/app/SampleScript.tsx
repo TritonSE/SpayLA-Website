2e374705206db50002db90099890738d51089a2a
anirudh9280 <anirudh.annabathula@gmail.com>
2025-02-22 23:32:12 -0800

    fixed the styling for HelpOutToday, SampleScript, and Call your local Politicians

--- frontend/src/app/SampleScript.tsx
+++ frontend/src/app/SampleScript.tsx
@@ -5,29 +5,28 @@ export default function SampleScript(): JSX.Element {
   return (
     <div className={styles.sampleScriptGrid}>
       <div className={styles.sampleScriptText}>
-        <h2>
-          Sample Script
+        <div className={styles.innerContainer}>
           <Image
-            src="/community_support/phone.svg"
-            className={`${styles.icon}`}
-            width={47}
-            height={52.17}
+            src="/community_support/phone_icon.svg"
             alt="Phone Icon"
+            width={20}
+            height={20}
+            className={styles.icon}
           />
-        </h2>
-        <Image
-          src="/community_support/yarn.svg"
-          width={184}
-          height={70.59}
-          className={`${styles.icon} ${styles.stringIcon}`}
-          alt="String Icon"
-        />
-        <p>
-          Hello my name is _______ and I want to leave a message for ______ regarding increasing
-          funding for spay and neuter clinics.
-        </p>
+          <h2>Sample Script</h2>
+          <p>
+            "Hello my name is _______ and I want to leave a message for ______ regarding increasing
+            funding for spay and neuter clinics in LA County".
+          </p>
+        </div>
       </div>
-      <Image src="/community_support/dog.png" width={721} height={493} alt="Dog" />
+      <Image
+        src="/community_support/dog.png"
+        width={720}
+        height={493}
+        alt="Dog"
+        style={{ width: "100%", height: "100%", objectFit: "cover" }}
+      />
     </div>
   );
 }
