7124875dd8000ea636a63a740c4f471351b45346
Syu125 <yusophia2011@gmail.com>
2025-03-01 17:44:44 -0800

    Created newsletter card component and added contents

---
+++ frontend/src/app/newsletters/page.tsx
@@ -0,0 +1,11 @@
+import PreviewCard from "@/components/newsletters/previewCard";
+
+export default function Newsletters() {
+  return (
+    <main>
+      <div>
+        <PreviewCard filepath="" date="03/01/2025"></PreviewCard>
+      </div>
+    </main>
+  );
+}
---
+++ frontend/src/components/newsletters/previewCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./previewCard.module.css";
+
+type previewCardProps = {
+  //   filepath: string;
+  date: string;
+};
+
+const PreviewCard: React.FC<previewCardProps> = ({ date }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.content}>
+        <div className={styles.thumbnail}>
+          <button className={styles.previewButton}>Preview</button>
+        </div>
+        <div className={styles.cardFooter}>
+          <div className={styles.date}>{date}</div>
+          <button className={styles.discardButton}>
+            <img className={styles.icon} src="/discardIcon.svg" alt=""></img>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PreviewCard;
