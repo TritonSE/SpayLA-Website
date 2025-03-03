6291e977b2a0be1df3a5e66d579e52b25e120708
Syu125 <yusophia2011@gmail.com>
2025-03-03 14:20:37 -0800

    format grid and add text

--- frontend/src/app/newsletters/page.tsx
+++ frontend/src/app/newsletters/page.tsx
@@ -1,10 +1,22 @@
+"use client";
+
+import { Button } from "@tritonse/tse-constellation";
+
+import styles from "./page.module.css";
+
 import PreviewCard from "@/components/newsletters/previewCard";
 
 export default function Newsletters() {
   return (
-    <main>
-      <div>
-        <PreviewCard date="03/01/2025"></PreviewCard>
+    <main className={styles.page}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Edit Newsletter</h1>
+        <Button></Button>
+        <div className={styles.grid}>
+          {Array.from({ length: 8 }, (_, index) => (
+            <PreviewCard key={index} date="3/01/2025"></PreviewCard>
+          ))}
+        </div>
       </div>
     </main>
   );
