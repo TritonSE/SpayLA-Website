d6571af11cc3740445b00cb634a0fa35c84ce04e
Argsweet <argsweet0628@gmail.com>
2025-04-17 17:31:14 -0700

    merged differences, added hover

--- frontend/src/app/newsletters/page.tsx
+++ frontend/src/app/newsletters/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+// import { useState} from "react";
 import { Button } from "@tritonse/tse-constellation";
 import * as pdfjsLib from "pdfjs-dist";
 import { useRef, useState } from "react";
--- frontend/src/components/newsletters/previewCard.tsx
+++ frontend/src/components/newsletters/previewCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 import styles from "./previewCard.module.css";
 
@@ -10,10 +11,12 @@ type previewCardProps = {
 };
 
 const PreviewCard: React.FC<previewCardProps> = ({ onPreview, date, children }) => {
+  const [isHovered, setIsHovered] = useState(false); //
   return (
     <div className={styles.card}>
       <div className={styles.content}>
         <div className={styles.thumbnail}>
+          {/* <button className={styles.previewButton}>Preview</button> */}
           <button className={styles.previewButton} onClick={onPreview}>
             Preview
           </button>
@@ -21,8 +24,20 @@ const PreviewCard: React.FC<previewCardProps> = ({ onPreview, date, children }) 
         </div>
         <div className={styles.cardFooter}>
           <div className={styles.date}>{date} Newsletter</div>
-          <button className={styles.discardButton}>
-            <img className={styles.icon} src="/discardIcon.svg" alt=""></img>
+          <button
+            className={styles.discardButton}
+            onMouseEnter={() => {
+              setIsHovered(true);
+            }} //
+            onMouseLeave={() => {
+              setIsHovered(false);
+            }} //
+          >
+            <img
+              className={styles.icon}
+              src={isHovered ? "/selected_trash.svg" : "/discardIcon.svg"}
+              alt="" //
+            />
           </button>
         </div>
       </div>
