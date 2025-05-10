3cdf05183fecc989a2c01e3988358440fdb63235
Syu125 <yusophia2011@gmail.com>
2025-05-09 22:36:34 -0700

    Finished frontend on main page

--- frontend/src/components/newsletter_display/newsletterDisplay.tsx
+++ frontend/src/components/newsletter_display/newsletterDisplay.tsx
@@ -10,6 +10,9 @@ export default function NewsletterDisplay() {
     { date: "2025-01-01", title: "01/01/2025 Newsletter", url: "some_url_1" },
     { date: "2024-12-15", title: "12/15/2024 Newsletter", url: "some_url_2" },
     { date: "2024-11-10", title: "11/10/2024 Newsletter", url: "some_url_3" },
+    { date: "2025-01-01", title: "01/01/2025 Newsletter", url: "some_url_1" },
+    { date: "2024-12-15", title: "12/15/2024 Newsletter", url: "some_url_2" },
+    { date: "2024-11-10", title: "11/10/2024 Newsletter", url: "some_url_3" },
     { date: "2024-08-20", title: "08/20/2024 Newsletter", url: "some_url_4" },
     { date: "2024-07-01", title: "07/01/2024 Newsletter", url: "some_url_5" },
     { date: "2024-04-15", title: "04/15/2024 Newsletter", url: "some_url_6" },
@@ -65,94 +68,96 @@ export default function NewsletterDisplay() {
   return (
     <main>
       <div className={styles.section}>
-        <div className={styles.latest}>
-          <div className={styles.header}>
-            <div className={styles.latestTitle}>Latest News</div>
-            <p className={styles.description}>Check out our most recent newsletter below!</p>
-          </div>
-
-          <div className={styles.previewCard}>
-            <button
-              className={styles.thumbnail}
-              onClick={() => {
-                openModal("some url");
-              }}
-            >
-              <img src={"some url"} alt="url" style={{ width: "100%" }} />
-            </button>
-          </div>
-        </div>
-        <div className={styles.rightSection}>
-          <div className={styles.previous}>
+        <div className={styles.sectionContent}>
+          <div className={styles.latest}>
             <div className={styles.header}>
-              <div className={styles.previousTitle}>In Case You Missed It,</div>
-              <p className={styles.description}>Catch up on our previous newsletters below!</p>
+              <div className={styles.latestTitle}>Latest News</div>
+              <p className={styles.description}>Check out our most recent newsletter below!</p>
             </div>
-            <div className={styles.content}>
-              <div className={styles.timeSelect}>
-                <Button
-                  className={`${styles.timespan} ${filterRange === "6months" ? styles.active : ""}`}
-                  onClick={() => {
-                    setFilterRange("6months");
-                    setCurrentPage(1);
-                  }}
-                >
-                  Past 6 months
-                </Button>
-                <Button
-                  className={`${styles.timespan} ${filterRange === "1year" ? styles.active : ""}`}
-                  onClick={() => {
-                    setFilterRange("1year");
-                    setCurrentPage(1);
-                  }}
-                >
-                  Past year
-                </Button>
+
+            <div className={styles.previewCard}>
+              <button
+                className={styles.thumbnail}
+                onClick={() => {
+                  openModal("some url");
+                }}
+              >
+                <img src={"some url"} alt="url" style={{ width: "100%" }} />
+              </button>
+            </div>
+          </div>
+          <div className={styles.rightSection}>
+            <div className={styles.previous}>
+              <div className={styles.header}>
+                <div className={styles.previousTitle}>In Case You Missed It,</div>
+                <p className={styles.description}>Catch up on our previous newsletters below!</p>
               </div>
-              <div className={styles.newsletterList}>
-                {currentNewsletters.map((newsletter, index) => (
+              <div className={styles.content}>
+                <div className={styles.timeSelect}>
                   <Button
-                    key={index}
-                    className={styles.newsletterView}
+                    className={`${styles.timespan} ${filterRange === "6months" ? styles.active : ""}`}
                     onClick={() => {
-                      openModal(newsletter.url);
+                      setFilterRange("6months");
+                      setCurrentPage(1);
                     }}
                   >
-                    {newsletter.title}
+                    Past 6 months
                   </Button>
-                ))}
+                  <Button
+                    className={`${styles.timespan} ${filterRange === "1year" ? styles.active : ""}`}
+                    onClick={() => {
+                      setFilterRange("1year");
+                      setCurrentPage(1);
+                    }}
+                  >
+                    Past year
+                  </Button>
+                </div>
+                <div className={styles.newsletterList}>
+                  {currentNewsletters.map((newsletter, index) => (
+                    <Button
+                      key={index}
+                      className={styles.newsletterView}
+                      onClick={() => {
+                        openModal(newsletter.url);
+                      }}
+                    >
+                      {newsletter.title}
+                    </Button>
+                  ))}
+                </div>
               </div>
-            </div>
-            <div className={styles.pageSelector}>
-              <Button
-                className={`${styles.pageButton} ${currentPage === 1 ? styles.disabledButton : ""}`}
-                disabled={currentPage === 1}
-                onClick={() => {
-                  setCurrentPage((p) => p - 1);
-                }}
-              >
-                <img src="/leftArrow.svg" alt="leftArrow" />
-              </Button>
-              {Array.from({ length: totalPages }, (_, i) => (
+              <div className={styles.pageSelector}>
                 <Button
-                  key={i}
-                  className={`${styles.button} ${currentPage === i + 1 ? styles.activePage : ""}`}
+                  className={`${styles.pageButton} ${currentPage === 1 ? styles.disabledButton : ""}`}
+                  disabled={currentPage === 1}
                   onClick={() => {
-                    setCurrentPage(i + 1);
+                    setCurrentPage((p) => p - 1);
                   }}
                 >
-                  {i + 1}
+                  <img src="/leftArrow.svg" alt="leftArrow" />
                 </Button>
-              ))}
-              <Button
-                className={`${styles.pageButton} ${currentPage === totalPages ? styles.disabledButton : ""}`}
-                disabled={currentPage === totalPages}
-                onClick={() => {
-                  setCurrentPage((p) => p + 1);
-                }}
-              >
-                <img src="/rightArrow.svg" alt="rightArrow" />
-              </Button>
+                {Array.from({ length: totalPages }, (_, i) => (
+                  <Button
+                    key={i}
+                    className={`${styles.button} ${currentPage === i + 1 ? styles.activePage : ""}`}
+                    onClick={() => {
+                      setCurrentPage(i + 1);
+                    }}
+                  >
+                    {i + 1}
+                  </Button>
+                ))}
+                <Button
+                  className={`${styles.pageButton} ${currentPage === totalPages ? styles.disabledButton : ""}`}
+                  disabled={currentPage === totalPages}
+                  onClick={() => {
+                    setCurrentPage((p) => p + 1);
+                  }}
+                >
+                  <img src="/rightArrow.svg" alt="rightArrow" />
+                </Button>
+              </div>
             </div>
           </div>
         </div>
