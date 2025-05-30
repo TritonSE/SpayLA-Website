b1ef51e287d6c1dc97396ad8578038ff4d38b6dc
Argsweet <argsweet0628@gmail.com>
2025-05-30 12:08:31 -0700

    attempt fix

--- frontend/src/app/newsletters/page.tsx
+++ frontend/src/app/newsletters/page.tsx
@@ -2,11 +2,9 @@
 // import { useState} from "react";
 import dynamic from "next/dynamic";
 
-const NewsletterClient = dynamic(() => import("./newsletteradminClient"), {
+const NewsletterClient = dynamic(() => import("./newsletterClient"), {
   ssr: false,
 });
-
-
 
 export default function NewslettersPage() {
   return <NewsletterClient />;
