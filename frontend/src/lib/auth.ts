3f582b83a3214bbfab411e73b1eeb049829a5e00
Yixuan Li <54194100+yxli001@users.noreply.github.com>
2025-11-02 23:24:45 -0800

    Auth integration (#63)
    * integrate login page with correct redirections
    
    * frontend auth complete
    
    * setup firebase on backend, add authenticated middleware

---
+++ frontend/src/lib/auth.ts
@@ -0,0 +1,62 @@
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from "./firebase";
+
+import type { User } from "firebase/auth";
+
+type ResponseError = {
+  field: "email" | "password" | "general";
+  message: string;
+};
+
+type FirebaseResponse =
+  | {
+      success: true;
+      user: User;
+    }
+  | {
+      success: false;
+      error: ResponseError;
+    };
+
+/* Returns user, or string error message if an error occurred.  */
+export async function login(email: string, password: string): Promise<FirebaseResponse> {
+  try {
+    const userCreds = await signInWithEmailAndPassword(auth, email, password);
+
+    return {
+      success: true,
+      user: userCreds.user,
+    };
+  } catch (error) {
+    const err = {
+      field: "general",
+      message: "An unknown error occurred.",
+    } as ResponseError;
+
+    if (!(error instanceof FirebaseError)) {
+      return {
+        success: false,
+        error: err,
+      };
+    }
+
+    if (error.code === "auth/user-not-found") {
+      err.field = "email";
+      err.message = "No user found with this email.";
+    } else if (error.code === "auth/invalid-credential" || error.code === "auth/wrong-password") {
+      err.field = "password";
+      err.message = "Invalid credentials.";
+    }
+
+    return {
+      success: false,
+      error: err,
+    };
+  }
+}
+
+export async function logout() {
+  await auth.signOut();
+}
---
+++ frontend/src/util/env.ts
@@ -0,0 +1,24 @@
+/**
+ * Parses .env parameters and ensures they are of required types and are not missing.
+ * If any .env parameters are missing or of the wrong type, the an error will be thrown.
+ */
+
+import { cleanEnv } from "envalid";
+import { json, str } from "envalid/dist/validators";
+
+import type { FirebaseOptions } from "firebase/app";
+
+/**
+ * NextJS only allows the frontend to access environment variables if they start with
+ * "NEXT_PUBLIC", so we have to manually acccess attributes of process.env here.
+ */
+export default cleanEnv(
+  {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_FIREBASE_CONFIG: process.env.NEXT_PUBLIC_FIREBASE_CONFIG,
+  },
+  {
+    NEXT_PUBLIC_BACKEND_URL: str(), // URL of our backend
+    NEXT_PUBLIC_FIREBASE_CONFIG: json<FirebaseOptions>(), // Firebase settings for frontend, stored as a JSON string
+  },
+);
