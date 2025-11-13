87d973021ea84277f07d6c06a6f6b2181faccb35
Philip Chen <johndoe@example.com>
2025-11-12 20:37:26 -0800

    fixes

--- backend/src/controllers/subscriber.ts
+++ backend/src/controllers/subscriber.ts
@@ -1,27 +1,28 @@
 import { matchedData, validationResult } from "express-validator";
+import createHttpError from "http-errors";
 
-import type { Subscriber as SubscriberType, SubscriberIds } from "../types/subscriber";
 import Subscriber from "../models/subscriber";
 import validationErrorParser from "../utils/validationErrorParser";
 
+import type { SubscriberIds, Subscriber as SubscriberType } from "../types/subscriber";
 import type { RequestHandler } from "express";
 
 export const createSubscriber: RequestHandler = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const errorMessage = validationErrorParser(errors);
-    return res.status(400).json({ message: errorMessage });
+    return next(createHttpError(400, errorMessage));
   }
   const data = matchedData(req) as unknown as SubscriberType;
   const { name, email } = data;
   try {
     const existingSubscriber = await Subscriber.findOne({ email });
     if (existingSubscriber) {
-      return res.status(409).json({ message: "Subscriber already exists" });
+      return next(createHttpError(409, "Subscriber already exists"));
     }
 
     const newSubscriber = await Subscriber.create({ name, email });
-    res.status(201).json({ message: "Subscriber created", subscriber: newSubscriber });
+    res.status(201).json(newSubscriber);
   } catch (error) {
     next(error);
   }
@@ -40,7 +41,7 @@ export const deleteSubscribers: RequestHandler = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const errorMessage = validationErrorParser(errors);
-    return res.status(400).json({ message: errorMessage });
+    return next(createHttpError(400, errorMessage));
   }
   const data = matchedData(req) as unknown as SubscriberIds;
   const { ids } = data;
@@ -51,10 +52,10 @@ export const deleteSubscribers: RequestHandler = async (req, res, next) => {
     });
 
     if (result.deletedCount === 0) {
-      return res.status(404).json({ message: "No subscribers found with the provided IDs." });
+      return next(createHttpError(404, "No subscribers found with the provided IDs."));
     }
 
-    res.status(200).json({ message: `${result.deletedCount} subscriber(s) deleted sucessfully.` });
+    res.status(204).send();
   } catch (error) {
     next(error);
   }
