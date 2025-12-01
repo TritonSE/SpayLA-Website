fc09db75b4eb83818520e257a2905983607833b2
yxli001 <lxd520anna@gmail.com>
2025-12-01 12:27:33 -0800

    update mission statement and contact info

--- frontend/src/components/WhatIsSpayLa.tsx
+++ frontend/src/components/WhatIsSpayLa.tsx
@@ -38,10 +38,9 @@ export default function WhatIsSpayLA() {
           fontFamily: "Open Sans, sans-serif",
         }}
       >
-        Spay.LA aims to combat animal overpopulation by creating <b>available</b>, <br />
-        <b>accessible</b>, and <b>affordable</b> spay and neuter clinics for the millions of pets{" "}
-        <br />
-        and community cats in Los Angeles.
+        Spay.LA aims to combat animal overpopulation by creating
+        <b>affordable</b>, <b>available</b>, and <b>accessible</b> spay/neuter clinics for the
+        millions of street cats and stray dogs in Los Angeles.
       </p>
 
       {/* Flex container pinned to bottom-right for both silhouettes */}
